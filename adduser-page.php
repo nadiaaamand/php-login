5eee1847c3cab3a1f9106e0a1eeee033048198c3
Nadia Aamand <nadiaaamand@gmail.com>
2016-09-29 12:09:58 +0200

    Login-logout
    function is now working

--- adduser-page.php
+++ adduser-page.php
@@ -12,15 +12,23 @@
 			echo "You are not logged in";
 		}
 ?>
-<br><br> 
-<!--Sign up form, needing full name and username + password. Then by using the action (where it directs to another page) and POST method it directs the info to the database-->
-<form action="adduser.php" method="POST">
-	<input type="text" name="first" placeholder="First Name"><br>
-	<input type="text" name="last" placeholder="Last Name"><br>
-    <input type="text" name="uid" placeholder="Username"><br>
-    <input type="password" name="pwd" placeholder="Password"><br>
-    <button type="submit">Sign Up</button>
-</form>
+<?php
+//Calling for the user id - if you are logged in you will see the id, if not you will get the else message
+	if (isset($_SESSION['ID'])) {
+		echo "You are already logged in!";
+		} else {
+			echo //Sign up form, needing full name and username + password. Then by using the action (where it directs to another page) and POST method it directs the info to the database
+			"<form action='adduser.php' method='POST'>
+				<input type='text' name='first placeholder='First Name'><br>
+				<input type='text' name='last' placeholder='Last Name'><br>
+				<input type='text' name='uid' placeholder='Username'><br>
+				<input type='password' name='pwd' placeholder='Password'><br>
+				<button type='submit'>Sign Up</button>
+		</form>";
+		}
+?>
+
+
  
 </body>
 </html>
